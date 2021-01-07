5148571b689592712b5321c147e1edfd07b4a555
Sansoldino <soldo.andrej@gmail.com>
2021-01-07 12:20:02 +0100

    Implement simple transfroms for Renderer2D Api

--- DreamTools/src/DreamTools/Renderer/Shader.h
+++ DreamTools/src/DreamTools/Renderer/Shader.h
@@ -1,6 +1,7 @@
 #pragma once
-
 #include <unordered_map>
+#include <glm/glm.hpp>
+
 namespace DreamTools
 {
 	class Shader
@@ -10,6 +11,10 @@ namespace DreamTools
 
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
+
+		virtual void SetFloat3(const std::string& name, const glm::vec3& value) = 0;
+		virtual void SetFloat4(const std::string& name, const glm::vec4& value) = 0;
+		virtual void SetMat4(const std::string& name, const glm::mat4& value) = 0;
 
 		virtual const std::string& GetName() const = 0;
 
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -6,7 +6,6 @@
 #include "../glm/glm/gtc/matrix_transform.hpp"
 #include "../glm/glm/gtc/type_ptr.hpp"
 
-#include "Platform/OpenGL/OpenGLShader.h"
 #include "../vendor/GLFW/include/GLFW/glfw3.h"
 
 namespace DreamTools
@@ -33,11 +32,8 @@ namespace DreamTools
 		DreamTools::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
 		DreamTools::Renderer2D::DrawQuad({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.5f, 0.2f, 1.0f });
+		DreamTools::Renderer2D::DrawQuad({ 0.0f, 1.0f, 0.0f }, { 1.0f, 1.0f }, { 0.4f, 0.1f, 0.7f, 1.0f });
 		DreamTools::Renderer2D::EndScene();
-		//ToDo: Add these functions Shader::SetMat4, Shader::SetFloat4
-		/*std::dynamic_pointer_cast<DreamTools::OpenGLShader>(m_FlatColorShader)->Bind();
-		std::dynamic_pointer_cast<DreamTools::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);*/
-
 	}
 	void Sandbox2D::OnImGuiRender()
 	{
