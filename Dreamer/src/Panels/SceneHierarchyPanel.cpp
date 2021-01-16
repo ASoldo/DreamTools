d667f629e61dec4a243fb6d917ffac738c61c9c6
Sansoldino <soldo.andrej@gmail.com>
2021-01-16 01:55:16 +0100

    Implement Properties Panel

--- Dreamer/src/Panels/SceneHierarchyPanel.cpp
+++ Dreamer/src/Panels/SceneHierarchyPanel.cpp
@@ -1,6 +1,7 @@
 //#include "dtpch.h"
 #include "SceneHierarchyPanel.h"
 #include "imgui/imgui.h"
+#include "glm/gtc/type_ptr.hpp"
 #include "DreamTools/Scene/Components.h"
 
 namespace DreamTools
@@ -12,10 +13,11 @@ namespace DreamTools
 	void SceneHierarchyPanel::SetContext(const Ref<Scene>& context)
 	{
 		m_Context = context;
+		m_SelectionContext = {};
 	}
 	void SceneHierarchyPanel::OnImGuiRender()
 	{
-		ImGui::Begin("Scene Hierarchy");
+		ImGui::Begin("Scene Hierarchy:");
 
 		m_Context->m_Registry.each([&](auto entityID) 
 		{
@@ -23,6 +25,18 @@ namespace DreamTools
 				DrawEntityNode(entity);
 		});
 
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+		{
+			m_SelectionContext = {};
+		}
+
+		ImGui::End();
+
+		ImGui::Begin("Properties:");
+		if (m_SelectionContext)
+		{
+			DrawComponents(m_SelectionContext);
+		}
 		ImGui::End();
 	}
 	void SceneHierarchyPanel::DrawEntityNode(Entity entity)
@@ -34,7 +48,7 @@ namespace DreamTools
 
 		ImGuiTreeNodeFlags flags = ((m_SelectionContext == entity) ? ImGuiTreeNodeFlags_Selected : 0)| ImGuiTreeNodeFlags_OpenOnArrow;
 		bool opened = ImGui::TreeNodeEx((void*)(uint64_t)(uint32_t)entity, flags, tag.c_str());
-		if (ImGui::IsItemClicked)
+		if (ImGui::IsItemClicked())
 		{
 			m_SelectionContext = entity;
 		}
@@ -48,6 +62,29 @@ namespace DreamTools
 				ImGui::TreePop();
 			}
 			ImGui::TreePop();
+		}
+	}
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+			static char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.5f);
+				ImGui::TreePop();
+			}
 		}
 	}
 }
--- Dreamer/src/Panels/SceneHierarchyPanel.h
+++ Dreamer/src/Panels/SceneHierarchyPanel.h
@@ -13,9 +13,11 @@ namespace DreamTools
 		SceneHierarchyPanel(const Ref<Scene>& context);
 		void SetContext(const Ref<Scene>& context);
 		void OnImGuiRender();
+		Entity GetSelectedEntity() const { return m_SelectionContext; }
 
 	private:
 		void DrawEntityNode(Entity entity);
+		void DrawComponents(Entity entity);
 
 	private:
 		Ref<Scene> m_Context;
